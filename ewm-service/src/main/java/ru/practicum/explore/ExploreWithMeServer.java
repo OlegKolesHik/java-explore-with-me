c12ce4de27c91664ea9da08a5b33019b9994594f
OlegKolesnik <0.Kolesnik.mos@gmail.com>
2023-02-03 19:46:28 +0300

    fix

--- ewm-service/src/main/java/ru/practicum/explore/ExploreWithMeServer.java
+++ ewm-service/src/main/java/ru/practicum/explore/ExploreWithMeServer.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ExploreWithMe {
+public class ExploreWithMeServer {
     public static void main(String[] args) {
-        SpringApplication.run(ExploreWithMe.class, args);
+        SpringApplication.run(ExploreWithMeServer.class, args);
     }
 }
 
