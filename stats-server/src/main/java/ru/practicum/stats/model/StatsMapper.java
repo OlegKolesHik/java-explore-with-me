63220ea0789326e1b7186ab47c6c07b8f6aeeb5a
OlegKolesnik <0.Kolesnik.mos@gmail.com>
2023-02-04 17:18:12 +0300

    fix

--- stats-server/src/main/java/ru/practicum/stats/model/StatsMapper.java
+++ stats-server/src/main/java/ru/practicum/stats/model/StatsMapper.java
@@ -1,12 +1,16 @@
 package ru.practicum.stats.model;
 
+import java.util.Collection;
 import java.util.List;
+
+import static java.util.Collections.reverse;
+import static org.hibernate.loader.internal.AliasConstantsHelper.get;
 
 public class StatsMapper {
     public static ViewStats toViewStats(List<Object> object) {
         return new ViewStats(
-                object.get(2).toString(),
+                object.get(0).toString(),
                 object.get(1).toString(),
-                Integer.reverse(Integer.parseInt(object.get(0).toString())));
+                Integer.parseInt(object.get(2).toString()));
     }
 }
