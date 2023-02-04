b25bb876245f5f4bad0ebe8e5207df432ed726ed
OlegKolesnik <0.Kolesnik.mos@gmail.com>
2023-02-04 18:35:59 +0300

    fix

--- stats-server/src/main/java/ru/practicum/stats/model/StatsMapper.java
+++ stats-server/src/main/java/ru/practicum/stats/model/StatsMapper.java
@@ -2,12 +2,14 @@ package ru.practicum.stats.model;
 
 import java.util.List;
 
+import static java.lang.Integer.parseInt;
+
 public class StatsMapper {
     public static ViewStats toViewStats(List<Object> object) {
         return new ViewStats(
                 object.get(2).toString(),
                 object.get(1).toString(),
-                Integer.reverse(Integer.parseInt((String) object.get(0))));
+                Integer.reverse(parseInt(object.get(0).toString())));
 
     }
 }
