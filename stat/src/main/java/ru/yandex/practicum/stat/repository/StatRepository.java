e6afbfa38467f1314fb5f934167b8790b643e6ea
OlegKolesnik <0.Kolesnik.mos@gmail.com>
2023-02-06 20:00:58 +0300

    fix

--- stat/src/main/java/ru/yandex/practicum/stat/repository/StatRepository.java
+++ stat/src/main/java/ru/yandex/practicum/stat/repository/StatRepository.java
@@ -13,14 +13,18 @@ import java.util.List;
 @Repository
 public interface StatRepository extends JpaRepository<Hit, Long> {
     @Query("SELECT NEW ru.yandex.practicum.stat.model.ViewStats(hit.app, hit.uri, COUNT(DISTINCT hit.uri)) " +
-            "FROM Hit hit" +
-            "WHERE hit.timeStamp BETWEEN :start AND :end AND s.uri IN :uris " +
+            "FROM Hit hit WHERE " +
+            "hit.timestamp >= :start " +
+            "AND hit.timestamp <= :end " +
+            "AND hit.uri IN :uris " +
             "GROUP BY hit.app, hit.uri ORDER BY COUNT(hit.id) DESC")
     List<ViewStats> getDistinctStats(LocalDateTime start, LocalDateTime end, String[] uris);
 
     @Query("SELECT  NEW ru.yandex.practicum.stat.model.ViewStats(hit.app, hit.uri, COUNT(hit.uri)) " +
-            "FROM Hit hit" +
-            "WHERE hit.timeStamp BETWEEN :start AND :end AND s.uri IN :uris " +
+            "FROM Hit hit WHERE " +
+            "hit.timestamp >= :start " +
+            "AND hit.timestamp <= :end " +
+            "AND hit.uri IN :uris " +
             "GROUP BY hit.app, hit.uri ORDER BY COUNT(hit.id) DESC")
     List<ViewStats> getStats(LocalDateTime start, LocalDateTime end, String[] uris);
 }
