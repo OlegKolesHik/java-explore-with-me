2bc155c39ad911b03fb4f8f1b2420f3c25239836
OlegKolesnik <0.Kolesnik.mos@gmail.com>
2023-02-19 15:31:41 +0300

    fix третьего этапа

--- main-service/src/main/java/ru/practicum/ewm/comments/model/Comment.java
+++ main-service/src/main/java/ru/practicum/ewm/comments/model/Comment.java
@@ -41,17 +41,6 @@ public class Comment {
                 '}';
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Comment)) return false;
-        return id != null && id.equals(((Comment) o).getId());
-    }
-
-    @Override
-    public int hashCode() {
-        return getClass().hashCode();
-    }
 
 }
 
